58a46fd43fce2d3ec7b333f46fdf75aa8e1ada78
j88moja-code <sitholent+github@gmail.com>
2022-10-30 02:47:47 -0700

    Add new test_base file

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""Defines unittest for models/base_model.py """
+
+from unittest import testcase
+import json
+import re
+from uuid import UUID, uuid4
+from datetime import datetime
+from time import sleep
+import test_models.tests_base_models
+
+
