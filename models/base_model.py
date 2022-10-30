336b7eab993af4fad7070910e907cb8247c7ce8b
j88moja-code <sitholent+github@gmail.com>
2022-10-30 05:18:39 -0700

    Update base model file

--- models/base_model.py
+++ models/base_model.py
@@ -1,47 +1,29 @@
 #!/usr/bin/python3
-''' module for BaseModel class '''
-
+import models
 import uuid
-import datetime
-import engine.file_storage as storage
+from datetime import datetime
 
 
-class BaseModel:
-       """Represents the BaseModel of the HBnB project."""
-
+class BaseModel():
+    '''class Base'''
     def __init__(self, *args, **kwargs):
-        """Initialize a new BaseModel.
-        Args:
-            *args (any): Unused.
-            **kwargs (dict): Key/value pairs of attributes.
-        """
-        tform = "%Y-%m-%dT%H:%M:%S.%f"
-        self.id = str(uuid4())
-        self.created_at = datetime.today()
-        self.updated_at = datetime.today()
-        if len(kwargs) != 0:
-            for k, v in kwargs.items():
-                if k == "created_at" or k == "updated_at":
-                    self.__dict__[k] = datetime.strptime(v, tform)
+        '''constructor class'''
+
+        if kwargs is not None and len(kwargs) != 0:
+            for key, value in kwargs.items():
+                if key == "__class__":
+                    continue
+                if key == "created_at" or key == "updated_at":
+                    self.__dict__[key] = datetime.now()
                 else:
-                    self.__dict__[k] = v
+                    setattr(self, key, value)
         else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
             models.storage.new(self)
 
-    def save(self):
-        ''' saves a model '''
-        self.updated_at = datetime.today()
-        models.storage.save()
-
-    def to_dict(self):
-        ''' returns a dictionary representation of the model '''
-        dct = self.__dict__.copy()
-        dct['__class__'] = self.__class__.__name__
-        dct['created_at'] = self.created_at.isoformat()
-        dct['updated_at'] = self.updated_at.isoformat()
-        return dct
-
     def __str__(self):
-        ''' returns a string representation of the model '''
-        return '[{}] ({}) {}'.format(
-            self.__class__.__name__, self.id, self.__dict__)
+        '''string representation'''
+        return (f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}")
+
