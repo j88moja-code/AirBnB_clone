6260437973f07759c690e4c666cfd7e447ad9465
Lebohang Motaung <lebohangmotaung88@gmail.com>
2022-10-30 03:01:29 -0700

    edited init file

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""__init__ magic method for models directory"""
-from models.engine.file_storage import FileStorage
+''' creates a static FileStorage instance '''
+import engine.file_storage
 
 
 storage = FileStorage()
