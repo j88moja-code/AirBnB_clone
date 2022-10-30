4a9e4cf9db5ddc27c97f8f42f90a51c13e4667c2
Lebohang Motaung <lebohangmotaung88@gmail.com>
2022-10-30 05:58:13 -0700

    edit file

--- console.py
+++ console.py
@@ -8,6 +8,7 @@ import json
 from models.engine.file_storage import FileStorage
 import os
 
+
 class HBNBCommand(cmd.Cmd):
     '''command interpreter'''
 
@@ -16,3 +17,23 @@ class HBNBCommand(cmd.Cmd):
     def do_quit(self, arg):
         '''to exit the program'''
         return True
+
+    def do_EOF(self, arg):
+        '''exit the program'''
+        return True
+
+    def help_quit(self):
+        '''updated and documented help'''
+        print('Quit command to exit the program')
+
+    def help_EOF(self):
+        '''updated and documented help'''
+        print('EOF commando to exit')
+
+    def emptyline(self):
+        '''empty line + ENTER shouldn't execute anything'''
+        pass
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
